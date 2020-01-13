65fcc233c38abb071f6c96f0ea9e5662834119f9
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-01-13 22:38:36 +0800

    can produce pdf so far

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pbys-receipts';
+
+  captureScreen() {
+    const data = document.getElementById('content');
+    html2canvas(data).then(canvas => {
+      const imgWidth = 215.9;
+      const pageHeight = 279.4;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'letter');
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('rawr.pdf');
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReceiptComponent } from './receipt/receipt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReceiptComponent
   ],
   imports: [
     BrowserModule,
