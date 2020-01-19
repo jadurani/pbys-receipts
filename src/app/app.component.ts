228cd33ca1ca541567a8358fa1bca352865a80cc
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-01-20 00:06:25 +0800

    Bugfix: Honor starting receipt number (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,15 @@ import html2canvas from 'html2canvas';
 })
 export class AppComponent {
   title = 'pbys-receipts';
-  startNum = 200;
-  pageTotal = 1;
-  currReceiptNum = this.startNum;
-  processing = false;
-  currPageNum = 1;
+  startNum: number;
+  pageNum: number;
+  pageTotal: number;
+  processing: boolean;
+  currPageNum: number;
 
-  constructor(private changeRef: ChangeDetectorRef) {}
+  constructor(private changeRef: ChangeDetectorRef) {
+    this.reset();
+  }
 
   async captureScreen() {
     this.processing = true;
@@ -26,25 +28,32 @@ export class AppComponent {
     const position = 0;
     const imgWidth = 215.9;
 
-    for (let pageNum = 0; pageNum < this.pageTotal; pageNum++) {
-      this.currPageNum = (pageNum + 1);
+    while(this.pageNum < this.pageTotal) {
+      this.currPageNum = (this.pageNum + 1);
 
       const data = document.getElementById('content');
       const canvas = await html2canvas(data, settings);
       const imgHeight = canvas.height * imgWidth / canvas.width;
-      // const heightLeft = imgHeight;
       const contentDataURL = canvas.toDataURL('image/png');
       pdf.addImage(contentDataURL, 'PNG', 2, position, imgWidth, imgHeight);
 
       if (this.currPageNum != this.pageTotal) {
         pdf.addPage();
-        this.currReceiptNum += 2;
-        this.changeRef.detectChanges();
       }
+      this.pageNum += 1;
+      this.changeRef.detectChanges();
     }
 
     pdf.save(`IPBYSP_receipts-${this.startNum}-${this.endNum}.pdf`);
+    this.reset();
+  }
+
+  reset() {
+    this.startNum = 200;
+    this.pageNum = 0;
+    this.pageTotal = 1;
     this.processing = false;
+    this.currPageNum = 1;
   }
 
   get endNum(): number {
@@ -56,5 +65,9 @@ export class AppComponent {
       return `Processing page ${this.currPageNum} of ${this.pageTotal}`;
     }
     return 'Capture Screen';
+  }
+
+  get currReceiptNum(): number {
+    return this.startNum + (this.pageNum * 2);
   }
 }
